9de2705bb1a773e7db384571ea7d8a24cb8a6af3
Payton Ireland <93439425+PAIreland@users.noreply.github.com>
2022-08-31 18:50:36 -0500

    Update API_Test.py

--- API_Test.py
+++ API_Test.py
@@ -45,6 +45,7 @@ def archive_contact(contact_id):
     return api.execute_request(contactsUrl + str(contact_id), api_request_object=data, method='PUT')
 
 # How to obtain application credentials: https://help.wildapricot.com/display/DOC/API+V2+authentication#APIV2authentication-Authorizingyourapplication
+# params in line below are API key and clinet secret
 api = API.WaApiClient("gc7fl5u5rt", "t3haoauwug3tm91h4cu44m6odi6skr")
 api.authenticate_with_contact_credentials("sch0010@uah.edu", "backend")
 accounts = api.execute_request("/v2/accounts")
