638d3096e026ff11137339ff5b56c487ff08dd12
sethhonea <42646121+sethhonea@users.noreply.github.com>
2022-09-11 17:28:18 -0500

    added more values to print for contact info
    added more values to print for contact info

--- API_Test.py
+++ API_Test.py
@@ -1,5 +1,6 @@
 #__author__ = 'dsmirnov@wildapricot.com'
 
+from tarfile import NUL
 import API
 import urllib.parse
 import json
@@ -14,28 +15,62 @@ def get_10_active_members():
     print(request_url)
     return api.execute_request(request_url).Contacts
 
-#GET 1 Active Member 
+# GET 1 Active Member 
 def get_1_active_member():
     params = {'$filter': 'member eq true',
               '$top': '1',
-              '$async': 'false'}
+              '$async': 'false',
+              'showSectionDividers': 'true'}
     request_url = contactsUrl + '?' + urllib.parse.urlencode(params)
     print(request_url)
     return api.execute_request(request_url).Contacts
 
-#PRINT CONTACT INFO
+# GET Custom Fields 
+def get_custom_fields():
+    params = {'showSectionDividers': 'false'}
+    request_url = contactsUrl + '?' + urllib.parse.urlencode(params)
+    print(request_url)
+    return api.execute_request(request_url).Contacts
+
+# GET Attachments   //// not currently working 9/11/22
+#def get_attachments():
+#    params = {'size': 'attachment eq true',
+#                'atBase64': 'false'}
+#    request_url = attachmentsUrl + '?' + urllib.parse.urlencode(params)
+#    print(request_url)
+#    return api.execute_request(request_url).Attachments
+
+# PRINT CONTACT INFO
 def print_contact_info(contact):
-    print('Contact details for ' + contact.DisplayName + ', ' + contact.Email)
-    print('Main info:')
-    print('\tID:' + str(contact.Id))
-    print('\tFirst name:' + contact.FirstName)
-    print('\tLast name:' + contact.LastName)
-    print('\tEmail:' + contact.Email)
-    print('\tAll contact fields:')
+    print('Contact details for.... ' + contact.DisplayName)
+    print('Main info: (values we can specifically grab from a contact)')
+    print('\tID: ' + str(contact.Id))
+    print('\tFirst name: ' + contact.FirstName)
+    print('\tLast name: ' + contact.LastName)
+    print('\tEmail: ' + contact.Email)
+    print('\tMembership Enabled: ' + str(contact.MembershipEnabled))
+    print('\tStatus: ' + contact.Status)
+    print('\tTerms of Use Accepted: ' + str(contact.TermsOfUseAccepted))
+    if(contact.Organization == ""):
+        contact.Organization = "NA"
+    print('\tOrganization: ' + contact.Organization)
+    print('\tProfile Last Updated: ' + contact.ProfileLastUpdated)
+    print('\tIs Account Administrator: ' + str(contact.IsAccountAdministrator))
+    
+    print('--------------------------------------')
+    print('\tAll contact fields where value is NOT "none":')
     for field in contact.FieldValues:
         if field.Value is not None:
-            print('\t\t' + field.FieldName + ':' + repr(field.Value))
+            print('\t\t' + field.FieldName + ': ' + repr(field.Value))
+     
+    print('--------------------------------------')
+    print('\tAll contact fields where value IS "none":')
+    for field in contact.FieldValues:
+        if field.Value is None:
+            print('\t\t' + field.FieldName + ': ' + repr(field.Value))
 
+    print(' ** End of Print Contact Info **')
+# end print_contact_info()
 
 #EXPORT CONTACT INFO
 def export_contact_info(contacts):
@@ -47,7 +82,6 @@ def export_contact_info(contacts):
         for contact in contacts:
             thewriter.writerow({'ID':contact.Id, 'First Name':contact.FirstName, 
             'Last Name':contact.LastName, 'Email':contact.Email})
-
 
 # CREATE A CONTACT
 def create_contact(email, name):
@@ -77,20 +111,33 @@ account = accounts[0]
 print(account.PrimaryDomainName)
 
 contactsUrl = next(res for res in account.Resources if res.Name == 'Contacts').Url
+#attachmentsUrl = next(res for res in account.Resources if res.Name == 'Attachments').Url
 
-# get top 10 active members and print their details
-#contacts = get_10_active_members()
+## get top 10 active members and print their details
+contacts = get_10_active_members()
+for contact in contacts:
+    print_contact_info(contact)
+
+## get top 1 active member and print their details (made for testing)
+#contacts = get_1_active_member()
 #for contact in contacts:
 #    print_contact_info(contact)
 
-## export contact info of members
-contacts = get_10_active_members()
-export_contact_info(contacts)
+#contacts_custom = get_custom_fields()
 
-# # create new contact
+## trying to get attachments
+#attachments = get_attachments()
+#for attachment in attachments:
+#    print_contact_info(contact)
+
+## EXPORT contact info of members
+#contacts = get_10_active_members()
+#export_contact_info(contacts)
+
+## create new contact
 # new_copntact = create_contact('some_email1@invaliddomain.org', 'John Doe')
 # print_contact_info(new_copntact)
 
-# # finally archive it
+## finally archive it
 # archived_contact = archive_contact(new_copntact.Id)
 # print_contact_info(archived_contact)
