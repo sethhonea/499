8122c2f3434c7ea329d67b8908c11e84a41573f0
sethhonea <42646121+sethhonea@users.noreply.github.com>
2022-09-27 00:25:48 -0500

    Event, Donation, Invoice
    Event, Donation, Invoice printing now

--- API_Test.py
+++ API_Test.py
@@ -34,6 +34,31 @@ def get_1_active_member():
     print(request_url)
     return api.execute_request(request_url).Contacts
 
+ # GET 1 Event
+def get_events():
+    params = {'$filter': 'RegistrationEnabled eq true',
+              '$top': '1',
+              '$async': 'false'}
+    request_url = eventsUrl + '?' + urllib.parse.urlencode(params)
+    print(request_url)
+    return api.execute_request(request_url).Events      
+
+# GET Invoices
+def get_invoices():
+    params = {'$unpaidOnly': 'true',
+              '$top': '1'}
+    request_url = invoicesUrl + '?' + urllib.parse.urlencode(params)
+    print(request_url)
+    return api.execute_request(request_url)
+
+# GET Donations
+def get_donations():
+    params = {'$top': '1'}
+    request_url = donationsUrl + '?' + urllib.parse.urlencode(params)
+    print(request_url)
+    return api.execute_request(request_url)
+
+
 # GET Attachments   //// not currently working 9/11/22
 #def get_attachments():
 #    params = {'size': 'attachment eq true',
@@ -41,6 +66,38 @@ def get_1_active_member():
 #    request_url = attachmentsUrl + '?' + urllib.parse.urlencode(params)
 #    print(request_url)
 #    return api.execute_request(request_url).Attachments
+
+# PRINT DONATION INFO
+def print_donation_info(payment):
+    print('Donation Value: ' + str(payment.Value))
+    print('Donation Date: ' + str(payment.DonationDate))
+    print('Donation made by: ' + str(payment.FirstName) + ' ' + str(payment.LastName))
+    if(payment.PublicComment == ""):
+        payment.PublicComment = "NA"
+    print('Donation Comments: ' + str(payment.PublicComment))
+    if(payment.Organization == ""):
+        payment.Organization = "NA"
+    print('Donation Organization: ' + str(payment.Organization))
+
+# PRINT INVOICE INFO
+def print_invoice_info(invoice):
+    print('Invoice ID: ' + str(invoice.Id))
+    print('Invoice Value: ' + str(invoice.Value))
+    print('Invoice Document Date: ' + str(invoice.DocumentDate))
+    print('Invoice Contact: ' + str(invoice.Contact))
+    print('Invoice Created Date: ' + str(invoice.CreatedDate))
+    print('Invoice Created By: ' + str(invoice.CreatedBy))
+    print('Invoice Paid: ' + str(invoice.IsPaid))
+    
+
+# PRINT EVENT INFO
+def print_event_info(event):
+    print('Event ID: ' + str(event.Id))
+    print('Event Name: ' + str(event.Name))
+    print('Event Start Date: ' + str(event.StartDate))
+    print('Event End Date: ' + str(event.EndDate))
+    print('Event Location: ' + str(event.Location))
+    
 
 # PRINT CONTACT INFO
 def print_contact_info(contact):
@@ -89,7 +146,8 @@ def print_contact_info(contact):
         if field.Value is None:
             print('\t\t' + field.FieldName + ': ' + repr(field.Value))
 
-    print(' ****** End of Print Contact Info ******')
+    print(' ** End of Print Contact Info **')
+    print('-----------------------------------------------------------------------------------------------')
 # end "print_contact_info()"
 
 # SET Contact Field Values
@@ -108,9 +166,7 @@ def get_contact_field_values():
     
     return c_field_values
 
-
-
-#EXPORT CONTACT INFO
+# EXPORT CONTACT INFO
 def export_contact_info(contacts):
    
     with open('exported_contact_info.csv', 'w', newline='') as csvfile:
@@ -190,29 +246,32 @@ account = accounts[0]
 
 print(account.PrimaryDomainName)
 
+## URLS to retrieve data
 contactsUrl = next(res for res in account.Resources if res.Name == 'Contacts').Url
-#attachmentsUrl = next(res for res in account.Resources if res.Name == 'Attachments').Url
+eventsUrl = next(res for res in account.Resources if res.Name == 'Events').Url
+invoicesUrl = next(res for res in account.Resources if res.Name == 'Invoices').Url
+donationsUrl = next(res for res in account.Resources if res.Name == 'Donations').Url
 
-## GET TOP 10 ACTIVE MEMBERS and print their details ##
+## GET TOP 10 ACTIVE MEMBERS and print their details ## (works)
 # contacts = get_10_active_members()
 # for contact in contacts:
 #     print_contact_info(contact)
 
-## GET 1 ACTIVE MEMBER (made for testing) ##
+## GET 1 ACTIVE MEMBER (made for testing) ## (works)
 #contacts = get_1_active_member()
 #for contact in contacts:
 #    print_contact_info(contact)
 #    set_contact_field_values(contact)
 
-## GET EXTRA CONTACT FIELD VALUES ##
+## GET EXTRA CONTACT FIELD VALUES ## (works?)
 #get_contact_field_values()
 
-## GET ATTACHMENTS (not working) ##
+## GET ATTACHMENTS (not working) ## (doesnt work)
 #attachments = get_attachments()
 #for attachment in attachments:
 #    print_contact_info(contact)
 
-## EXPORT CONTACT INFO ##
+## EXPORT CONTACT INFO ## (works)
 #contacts = get_10_active_members()
 #export_contact_info(contacts)
 
@@ -227,9 +286,29 @@ contactsUrl = next(res for res in account.Resources if res.Name == 'Contacts').U
 
 
 ## Next: pull info from exported_contact_info.csv and import it into database
-import_data()
-
+# import_data()
 
 ## FINALLY ARCHIVE IT ##
 # archived_contact = archive_contact(new_copntact.Id)
 # print_contact_info(archived_contact)
+
+## GET AND PRINT EVENTS ## (works)
+events = get_events()
+for event in events:
+    print('** BEGIN EVENT INFO **')
+    print_event_info(event)
+    print('** END EVENT INFO **')
+
+## GET AND PRINT INVOICES ## (works)
+invoices = get_invoices()
+for invoice in invoices:
+    print('** BEGIN INVOICE INFO **')
+    print_invoice_info(invoice)
+    print('** END INVOICE INFO **')
+
+## GET AND PRINT DONATIONS ## (works)
+donations = get_donations()
+for donation in donations:
+    print('** BEGIN DONATION INFO **')
+    print_donation_info(donation)
+    print('** END DONATION INFO **')
